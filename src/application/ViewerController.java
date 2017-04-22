5ea25a2a1ef1ceb61ed892120c17a3b44aaf55a4
oden <shumpmita@ic.dis.titech.ac.jp>
2017-04-22 22:52:57 +0900

    * チャート画面を保存できるよう修正

--- src/application/ViewerController.java
+++ src/application/ViewerController.java
@@ -3,13 +3,18 @@ package application;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
+import javax.imageio.ImageIO;
+
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.ScatterChart;
 import javafx.scene.chart.XYChart;
@@ -19,6 +24,8 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.Tooltip;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
 
 /**
  * @author shumpmita
@@ -47,6 +54,8 @@ public class ViewerController {
 	private Button backButton;
 	@FXML
 	private Button lastButton;
+	@FXML
+	private Button saveButton;
 
 	@FXML
 	private Slider slider;
@@ -57,8 +66,14 @@ public class ViewerController {
 
 	private static int index;
 
+	private FileChooser fileChooser;
+
 	@FXML
 	public void initialize() {
+		fileChooser = new FileChooser();
+		fileChooser.setTitle("チャート保存");
+		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png"));
+
 		xAxis.setAutoRanging(false);
 		xAxis.setLowerBound(-5);
 		xAxis.setUpperBound(5);
@@ -109,6 +124,7 @@ public class ViewerController {
 		assert noOfEvalList.size() == bestEvalList.size();
 		assert noOfEvalList.size() == dataList.size();
 
+		//1e-5と1(倍率)をeの非線形で結ぶ
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
 				double magValue = 1e-5 * Math.exp(new_val.doubleValue() * 5 * Math.log(10));
@@ -120,6 +136,24 @@ public class ViewerController {
 		});
 
 		onActionStartButton(null);
+	}
+
+	@FXML
+	protected void onSnapShotButtonAction(ActionEvent e) {
+		File importFile = fileChooser.showSaveDialog(saveButton.getScene().getWindow());
+		if (importFile != null) {
+			String path = importFile.getPath().toString();
+			WritableImage image = chart.snapshot(new SnapshotParameters(), null);
+
+			// TODO: probably use a file chooser here
+			File file = new File(path);
+
+			try {
+				ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+			} catch (IOException e1) {
+				// TODO: handle exception here
+			}
+		}
 	}
 
 	@FXML
