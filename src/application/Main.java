91b66638065b440fcbb76a785ee21b643bb0d158
oden <shumpmita@ic.dis.titech.ac.jp>
2017-05-18 15:17:30 +0900

    * コメント追加 * CanvasからChartに変更

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,8 +10,9 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			//FXMLLoader fxml = new FXMLLoader(getClass().getResource("./Viewer.fxml"));
-			FXMLLoader fxml = new FXMLLoader(getClass().getResource("./Viewer_Canvas.fxml"));
+			FXMLLoader fxml = new FXMLLoader(getClass().getResource("./Viewer.fxml"));
+			// FXMLLoader fxml = new
+			// FXMLLoader(getClass().getResource("./Viewer_Canvas.fxml"));
 			BorderPane root = fxml.load();
 
 			Scene scene = new Scene(root);
@@ -19,7 +20,8 @@ public class Main extends Application {
 			primaryStage.setTitle("Viewer");
 			primaryStage.setScene(scene);
 			primaryStage.show();
-		} catch (Exception e) {
+		}
+		catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
--- src/application/ViewerController.java
+++ src/application/ViewerController.java
@@ -7,8 +7,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.stream.Stream;
 
-import javax.imageio.ImageIO;
-
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
@@ -27,36 +25,44 @@ import javafx.scene.control.Tooltip;
 import javafx.scene.image.WritableImage;
 import javafx.stage.FileChooser;
 
+import javax.imageio.ImageIO;
+
 /**
  * @author shumpmita
  *
  */
 public class ViewerController {
-
+	// 評価回数のラベル
 	@FXML
 	private Label noOfEvalLabel;
-
+	// 最良評価値のラベル
 	@FXML
 	private Label bestEvalLabel;
-
+	// データを表示するチャート
 	@FXML
 	private ScatterChart<Double, Double> chart;
+	// x軸
 	@FXML
 	private NumberAxis xAxis;
+	// y軸
 	@FXML
 	private NumberAxis yAxis;
-
+	// 最初の世代のデータを表示するボタン
 	@FXML
 	private Button startButton;
+	// 次世代のデータを表示するボタン
 	@FXML
 	private Button nextButton;
+	// 前世代のデータを表示するボタン
 	@FXML
 	private Button backButton;
+	// 最終世代を表示するボタン
 	@FXML
 	private Button lastButton;
+	// チャートをpng形式で保存するボタン
 	@FXML
 	private Button saveButton;
-
+	// チャートの範囲を決定するスライダー
 	@FXML
 	private Slider slider;
 
@@ -64,8 +70,9 @@ public class ViewerController {
 	private ArrayList<Double> bestEvalList;
 	private ArrayList<double[][]> dataList;
 
+	// 世代数
 	private static int index;
-
+	// チャートを画像保存する際に選択するオブジェクト
 	private FileChooser fileChooser;
 
 	@FXML
@@ -74,6 +81,7 @@ public class ViewerController {
 		fileChooser.setTitle("チャート保存");
 		fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("Image Files", "*.png"));
 
+		// x軸，y軸の設定
 		xAxis.setAutoRanging(false);
 		xAxis.setLowerBound(-5);
 		xAxis.setUpperBound(5);
@@ -88,7 +96,7 @@ public class ViewerController {
 		bestEvalList = new ArrayList<>();
 		dataList = new ArrayList<>();
 
-		//logフォルダにあるデータを読み込み
+		// logフォルダにあるデータを読み込み
 		File file = new File("./log/BestValue.csv");
 		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
 			// オブジェクトに落とし込み
@@ -98,7 +106,8 @@ public class ViewerController {
 				noOfEvalList.add(Long.parseLong(split[0]));
 				bestEvalList.add(Double.parseDouble(split[1]));
 			}
-		} catch (Exception ex) {
+		}
+		catch (Exception ex) {
 			System.out.println("BestValue読み込みでエラー");
 		}
 
@@ -117,14 +126,16 @@ public class ViewerController {
 				}
 				dataList.add(array);
 			}
-		} catch (Exception ex) {
+		}
+		catch (Exception ex) {
 			System.out.println("Population読み込みでエラー");
 		}
 
 		assert noOfEvalList.size() == bestEvalList.size();
 		assert noOfEvalList.size() == dataList.size();
 
-		//1e-5と1(倍率)をeの非線形で結ぶ
+		// 1e-5と1(倍率)をeの非線形で結ぶ
+		// スライダーの変更と同時にチャート範囲を変更するイベントを追加
 		slider.valueProperty().addListener(new ChangeListener<Number>() {
 			public void changed(ObservableValue<? extends Number> ov, Number old_val, Number new_val) {
 				double magValue = 1e-5 * Math.exp(new_val.doubleValue() * 5 * Math.log(10));
@@ -134,23 +145,23 @@ public class ViewerController {
 				yAxis.setUpperBound(magValue * 5);
 			}
 		});
-
+		// 初期集団を表示
 		onActionStartButton(null);
 	}
 
+	// チャート保存ボタン押下時に呼び出されるメソッド
 	@FXML
 	protected void onSnapShotButtonAction(ActionEvent e) {
 		File importFile = fileChooser.showSaveDialog(saveButton.getScene().getWindow());
+		// 保存場所を設定していればnullでない
 		if (importFile != null) {
 			String path = importFile.getPath().toString();
 			WritableImage image = chart.snapshot(new SnapshotParameters(), null);
-
-			// TODO: probably use a file chooser here
 			File file = new File(path);
-
 			try {
 				ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
-			} catch (IOException e1) {
+			}
+			catch (IOException e1) {
 				// TODO: handle exception here
 			}
 		}
@@ -208,7 +219,9 @@ public class ViewerController {
 
 	/**
 	 * ラベルおよびチャートにデータを表示
-	 * @param index 世代
+	 *
+	 * @param index
+	 *                世代
 	 */
 	private void SetData(int index) {
 		noOfEvalLabel.setText(String.valueOf(noOfEvalList.get(index)));
@@ -231,20 +244,20 @@ public class ViewerController {
 			for (Data<Double, Double> point : s.getData()) {
 				double x = point.getXValue().doubleValue();
 				double y = point.getYValue().doubleValue();
-				Tooltip.install(point.getNode(),
-						new Tooltip(String.format("(%1.3e, %1.3e), Eval:%1.3e", x, y, ktablet(new double[] { x, y }))));
+				Tooltip.install(point.getNode(), new Tooltip(String.format("(%1.3e, %1.3e), Eval:%1.3e", x, y, ktablet(new double[] { x, y }))));
 			}
 		}
 	}
 
 	private static double ktablet(double[] x) {
-		int k = (int) ((double) x.length / 4.0); //k=n/4
-		double result = 0.0; //評価値を初期化
+		int k = (int) ((double) x.length / 4.0); // k=n/4
+		double result = 0.0; // 評価値を初期化
 		for (int i = 0; i < x.length; ++i) {
-			double xi = x[i]; //i番目の次元の要素
+			double xi = x[i]; // i番目の次元の要素
 			if (i < k) {
 				result += xi * xi;
-			} else {
+			}
+			else {
 				result += 10000.0 * xi * xi;
 			}
 		}
